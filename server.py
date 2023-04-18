22200012bc25053b7fd6aa056347865358147dfc
J1min <aoiharu41@gmail.com>
2023-04-18 09:55:26 +0900

    fix(db): 데이터베이스 함수 부

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import func, select
 
 from interface import model, schemas
+from util import db
 import database
 
 app = FastAPI()
@@ -20,22 +21,8 @@ app.add_middleware(
 )
 
 
-def get_db():
-    try:
-        db = database.SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-def post_db(db, data):
-    db.add(data)
-    db.commit()
-    db.refresh(data)
-
-
 @app.get("/quote")
-def get_all_quote(db: Session = Depends(get_db)):
+def get_all_quote(db: Session = Depends(db.get_db)):
     quote_list = db.query(model.quote).order_by(func.rand()).limit(1).first()
     random_quote = {
         "quote_content": quote_list.quote_content,
@@ -47,21 +34,21 @@ def get_all_quote(db: Session = Depends(get_db)):
 
 
 @app.get("/quote/all")
-def get_all_quote(db: Session = Depends(get_db)):
+def get_all_quote(db: Session = Depends(db.get_db)):
     quote_list = db.query(model.quote).all()
     return {"list": quote_list}
 
 
 @app.get("/quote/{quote_id}")
-def get_quote(quote_id: int, db: Session = Depends(get_db)):
+def get_quote(quote_id: int, db: Session = Depends(db.get_db)):
     quote = db.query(model.quote).filter(
         model.quote.quote_id == quote_id).first()
     return {"quote": quote}
 
 
 @app.post("/quote")
-def post_board(body: schemas.quote, db: Session = Depends(get_db)):
+def post_board(body: schemas.quote, db: Session = Depends(db.get_db)):
     quote_data = model.quote(
         quote_content=body.quote_content, charactor_name=body.charactor_name)
-    post_db(db, quote_data)
+    db.post_db(db, quote_data)
     return {"response": "추가 완료"}
